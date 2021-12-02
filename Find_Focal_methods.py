d19073a8885732da65c54861c9e1a55bdb8ec5cd
Sepehr <sepehrhashtroudi@gmail.com>
2021-12-01 19:40:42 -0700

    updated focal

--- Find_Focal_methods.py
+++ Find_Focal_methods.py
@@ -1,4 +1,7 @@
 
+import re
+
+
 test_file = "csv_tests/CSVFormat_ESTest.java"
 method_names = "method_names/CSVFormat.java"
 source_file = "methods/CSVFormat.java"
@@ -18,17 +21,10 @@ f.close()
 
 
 methods = methods.split('\n')
-m_names = m_names.split('\n')
 tests = tests.split('@Test')
-# print(sources)
-
-# print('@Test'.join(tests))
 method_name_list = []
-for m_name in m_names:
-    m_name = m_name.split(" ")
-    for i in m_name:
-        if "(" in i:
-            method_name_list.append(i.split("(")[0])
+method_name_list = [i.replace('(','') for i in re.findall('\w+\(',m_names)]
+# print(method_name_list)
 
 
 methods = dict(zip(method_name_list, methods))
@@ -38,12 +34,26 @@ method_test = {}
 for test in tests:
     for key,value in methods.items():
         if test.find(key)!=-1:
-            method_test["@Test" + test.replace('\n','\t')] = value.strip('\n')
+            test_s = test.split(';')
+            new_test = []
+            for i in test_s:
+                if i.find('assert') == -1 or i.find(key) != -1:
+                    new_test.append(i)
+            new_test = ';'.join(new_test)
+            test_name = re.findall('test\d+',new_test)[0]
+            method_test['@Test' + new_test.replace(test_name,"test"+key)] = value
+            
+
     
-for key , value in method_test.items():
-    print(key,value)
-    print('\n\n\n')
+# for key , value in method_test.items():
+#     print(key,value)
+#     print('\n\n\n')
     
+
+with open("extracted_tests.txt", "w") as f1, open("extracted_methods.txt", "w") as f2:
+    for key , value in method_test.items():
+        f1.write(key.replace('\n','\t') + '\n')
+        f2.write(value+'\n')
 
 
     
